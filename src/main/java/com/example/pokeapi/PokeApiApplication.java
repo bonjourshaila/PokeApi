1a7360c0934ae71f5d9c41b396bf04b1b67b41f4
Shaila Palafox <58263860+bonjourshaila@users.noreply.github.com>
2022-06-09 14:05:06 -0500

    poki api

---
+++ src/main/java/com/example/pokeapi/PokeApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.pokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class PokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
